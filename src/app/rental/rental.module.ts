5cb204f652a82ea80e971ac9e7e0eb0c15046101
Lena abu hatab <lenaabuhatab@gmail.com>
2018-12-18 13:30:45 +0200

    Add proxy configuration, add rental detials text from server, app ngx pipes

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+
+import { NgPipesModule } from "ngx-pipes";
 
 import { RentalComponent } from "./rental.component";
 import { RentalListComponent } from "./rental-list/rental-list.component";
 import { RentalListItemComponent } from "./rental-list/rental-list-item/rental-list-item.component";
+import { RentalDetailComponent } from "./rental-detail/rental-detail.component";
 
 import { RentalService } from "./shared/rental.service";
 
@@ -13,14 +17,24 @@ const routes: Routes = [
     path: "rentals",
     component: RentalComponent,
     children: [
-      { path: "", component: RentalListComponent }
-      // { path: ':rentalId', component: RentalDetailComponent}
+      { path: "", component: RentalListComponent },
+      { path: ":rentalId", component: RentalDetailComponent }
     ]
   }
 ];
 @NgModule({
-  declarations: [RentalComponent, RentalListComponent, RentalListItemComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    RentalComponent,
+    RentalListComponent,
+    RentalListItemComponent,
+    RentalDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    HttpClientModule,
+    NgPipesModule
+  ],
   providers: [RentalService]
 })
 export class RentalModule {}
